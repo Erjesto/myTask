b46dad57eee3f8f6e38954d53c61b4e18d438300
Erjesto <Ra267@mail.ru>
2021-07-13 19:49:26 +0300

    Try to fix

--- app.js
+++ app.js
@@ -1,92 +1,65 @@
-const name = document.getElementById('userName')
-userName.addEventListener('input', (e) => {
-    const chekName = /[^0-9]$/
-    const text = e.target.value
-    
+function checkFields() {
+    const username = document.getElementById('username').value
+    const surname = document.getElementById('surname').value
+    const password = document.getElementById('password').value
+    const email = document.getElementById('email').value
+    const phone = document.getElementById('phone').value
+    const address = document.getElementById('address').value
+    const form = document.getElementById('form').value
+    const errorElement = document.getElementById('error')
+    const submit = document.getElementById('submit') 
+    var text = ''
 
-    const result = chekName.test(text)
-    if (result) {
-        userName.style.border = 'green solid 2px'
-        
-        
-     } else {
-         userName.style.border = 'red solid 2px'
-     }
-})
-
-const surName = document.getElementById('userSurname') 
-userSurname.addEventListener('input', (e) => {
+    const checkName = /[^0-9]$/
     const checkSurname = /[^0-9]$/
-    const text = e.target.value
+    const checkPass = /[a-zA-Z0-9{8, 12}]/
+    const checkEmail = /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/
+    const checkPhone = /[^0-9\+\(\)]/
+    const checkAddress = /[a-zA-Z0-9.]/
 
-    const result = checkSurname.test(text)
-    if (result) {
-        userSurname.style.border = 'green solid 2px'
-     } else {
-         userSurname.style.border = 'red solid 2px'
-     }
-})
-
-const password = document.getElementById('password')
-password.addEventListener('input', (e) => {
-    const checkPass = /\d{8}/
-    const text = e.target.value
-    const result = checkPass.test(text)
-
-    if (result) {
-        password.style.border = 'green solid 2px'
-     } else {
-        password.style.border = 'red solid 2px'
-     }
-})
-
-const email = document.getElementById('email')
-email.addEventListener('input', (e) => {
-    const checkEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    const text = e.target.value
-    const result = checkEmail.test(text)
-    
-
-    if (result) {
-       email.style.border = 'green solid 2px'
-    } else {
-        email.style.border = 'red solid 2px'
+    if (!checkName.test(username)) {
+        text = 'invalid name'
+        errorElement.innerHTML = text
+        errorElement.style.display = 'block'
+        return false
     }
-})
 
-const phone = document.getElementById('phone')
-phone.addEventListener('input', (e) => {
-    const checkPhone = /^[+]{1}[0-9]{1} [(]{1}[0-9]{3}[)]{1} [0-9]{3} [0-9]{2} [0-9]{2}$/
-    const text = e.target.value
-    const result = checkPhone.test(text)
+    if (!checkSurname.test(surname)) {
+        text = 'invalid surname'
+        errorElement.innerHTML = text
+        errorElement.style.display = 'block'
+        return false
+    }
 
-    if (result) {
-        phone.style.border = 'green solid 2px'
-     } else {
-        phone.style.border = 'red solid 2px'
-     }
+    if (!checkPass.test(password)) {
+        text = 'invalid password'
+        errorElement.innerHTML = text
+        errorElement.style.display = 'block'
+        return false
+    }
 
-})
+    if (!checkEmail.test(email)) {
+        text = 'invalid email'
+        errorElement.innerHTML = text
+        errorElement.style.display = 'block'
+        return false
+    }
 
+    if (!checkPhone.test(phone)) {
+        text = 'invalid number'
+        errorElement.innerHTML = text
+        errorElement.style.display = 'block'
+        return false
+    }
 
-const address = document.getElementById('address')
-address.addEventListener('input', (e) => {
-    const checkAddress = /^[a-zA-Z0-9,\.\s]+$/
-    const text = e.target.value
-    const result = checkAddress.test(text)
+    if (!checkAddress.test(address)) {
+        text = 'invalid address'
+        errorElement.innerHTML = text
+        errorElement.style.display = 'block'
+        return false
+    }
 
-    if (result) {
-        address.style.border = 'green solid 2px'
-     } else {
-        address.style.border = 'red solid 2px'
-     }
-})
+    
+    return false
 
-
-
-
-
-
-
-
-
+}
